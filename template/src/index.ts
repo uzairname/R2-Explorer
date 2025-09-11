f81747bf91d4dc6a8323f6c1c816886002fc1504
Uzair <23709618+uzairname@users.noreply.github.com>
2025-09-11 13:46:26 -0400

    add username password

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,9 +3,9 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: true,
-	// basicAuth: {
-	//   username: 'username',
-	//   password: 'password'
-	// },
+	basicAuth: {
+	  username: 'username',
+	  password: 'password'
+	},
   cfAccessTeamName: '5r'
 });
