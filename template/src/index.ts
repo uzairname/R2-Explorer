79d16d87e8070fe3b4f990659820c3aaf664e0db
Uzair Mohammed <moham147@egr.msu.edu>
2025-09-12 15:52:37 -0400

    update config

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,7 +2,7 @@ import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
-	readonly: true,
+	readonly: false,
 	basicAuth: {
 	  username: 'username',
 	  password: 'password'
